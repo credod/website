8182a45d3c878b4d60a7e8d56d42382d41146784
Credo Duarte <credo_duarte@brown.edu>
2022-01-08 22:01:05 -0600

    updated readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
+    <>
+    <Head>
+      <title>Credo Duarte</title>
+    </Head>
     <div className={styles.container}>
       <h1>Website coming soon!</h1>
     </div>
+    </>
   )
 }
 
